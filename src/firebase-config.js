e4dbc8a0e2c498377005c4dd8589a0e5272f015d
Amornath <amor2017nstucste@gmail.com>
2022-01-07 00:46:25 +0600

    another update

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-    
+    // this code block is not available for security concern
   };
 
   const app = initializeApp(firebaseConfig);
